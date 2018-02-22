bc0f3ab445f60f8c2dfb4324cdb4c816ad027a2c
jared <jared.brinton@gmail.com>
2018-02-22 12:26:47 -0700

    Finished last parts, only debugging left

--- manager/views/edit.py
+++ manager/views/edit.py
@@ -21,7 +21,7 @@ def process_request(request):
     print('I FINISHED RENDERING THE FORM')
     if form.is_valid():
         print('THE FORM IS VALID')
-        form.commit()
+        form.commit(product)
         return HttpResponseRedirect('/manager/products')
 
     # render the template
@@ -36,61 +36,71 @@ class EditForm(Formless):
         self.product = product
         super().__init__(request)
 
+    #
+
     def init(self):
-        self.fields['name'] = forms.CharField(label='Name', initial=self.product.name)
-        self.fields['status'] = forms.ChoiceField(choices=self.product.STATUS_CHOICES, initial=self.product.status)
-        self.fields['TYPE_CHOICES'] = forms.ChoiceField(choices=self.product.TYPE_CHOICES, widget = forms.Select(attrs = {'onchange' : "showFields(true);",}))
-        self.fields['description'] = forms.CharField(label='Description', initial=self.product.description)
-        self.fields['category'] = forms.ChoiceField(choices=[[x.name, x.name] for x in cmod.Category.objects.all()], initial=self.product.category.name)
-        # self.fields['category2'] = forms.ModelChoiceField(queryset=cmod.Category.objects.all())
-        self.fields['price'] = forms.DecimalField(label='Price', decimal_places=2, initial=self.product.price)
+        self.fields['name'] = forms.CharField(label='Name', initial=self.product.name, required=True)
+        self.fields['status'] = forms.ChoiceField(choices=self.product.STATUS_CHOICES, initial=self.product.status, required=True)
+        if self.product.name == '':
+            self.fields['TYPE_CHOICES'] = forms.ChoiceField(choices=self.product.TYPE_CHOICES, required=True, widget = forms.Select(attrs={'onchange': "showFields(true);",}))
+        self.fields['description'] = forms.CharField(label='Description', initial=self.product.description, required=True)
+        # self.fields['category'] = forms.ChoiceField(choices=[[x.name, x.name] for x in cmod.Category.objects.all()], initial=self.product.category.name, required=True)
+        self.fields['category'] = forms.ModelChoiceField(queryset=cmod.Category.objects.all(), initial=self.product.category.name)
+        self.fields['price'] = forms.DecimalField(label='Price', decimal_places=2, initial=self.product.price, required=True)
 
         try:
             if self.product.polymorphic_ctype.name == 'bulk product':
                 prod = cmod.BulkProduct.objects.get(id=self.product.id)
-                self.fields['quantity'] = forms.IntegerField(label='Quantity', initial=prod.quantity)
+                self.fields['quantity'] = forms.IntegerField(label='Quantity', initial=prod.quantity, required=False)
             else:
-                self.fields['quantity'] = forms.IntegerField(label='Quantity')
+                self.fields['quantity'] = forms.IntegerField(label='Quantity', required=False)
         except:
-            self.fields['quantity'] = forms.IntegerField(label='Quantity')
+            self.fields['quantity'] = forms.IntegerField(label='Quantity', required=False)
 
         try:
             if self.product.polymorphic_ctype.name == 'rental product':
                 prod = cmod.RentalProduct.objects.get(id=self.product.id)
-                self.fields['max_rental_days'] = forms.IntegerField(label='Maximum Rental Period in Days', initial=prod.max_rental_days)
-                self.fields['retire_date'] = forms.DateTimeField(initial=prod.retire_date)
+                self.fields['max_rental_days'] = forms.IntegerField(label='Maximum Rental Period in Days', initial=prod.max_rental_days, required=False)
+                self.fields['retire_date'] = forms.DateTimeField(label='Retire Date', initial=prod.retire_date, required=False)
             else:
-                self.fields['max_rental_days'] = forms.IntegerField(label='Maximum Rental Period in Days')
-                self.fields['retire_date'] = forms.DateTimeField()
+                self.fields['max_rental_days'] = forms.IntegerField(label='Maximum Rental Period in Days', required=False)
+                self.fields['retire_date'] = forms.DateTimeField(label='Retire Date', required=False)
         except:
-            self.fields['max_rental_days'] = forms.IntegerField(label='Maximum Rental Period in Days')
-            self.fields['retire_date'] = forms.DateTimeField()
+            self.fields['max_rental_days'] = forms.IntegerField(label='Maximum Rental Period in Days', required=False)
+            self.fields['retire_date'] = forms.DateTimeField(label='Retire Date', required=False)
+
+    #
 
     def clean(self):
         print('I STARTED CLEANING')
-        # self.product = self.product
-        #
-        # if self.product is None:
-        #     raise forms.ValidationError('You should not be capable of committing this error')
-
         # return the cleaned data dict, per django spec
         return self.cleaned_data
 
-    def commit(self):
-        """Process the form action"""
-        if self.product.polymorphic_ctype.name == 'bulk product':
-            self.product.quantity = self.cleaned_data.get('quantity')
+    #
 
-        if self.product.polymorphic_ctype.name == 'rental product':
-            self.product.max_rental_days = self.cleaned_data.get('max_rental_days')
-            self.product.retire_date = self.cleaned_data.get('retire_date')
+    def commit(self, product):
+        """Process the form action"""
 
         self.product.name = self.cleaned_data.get('name')
         self.product.status = self.cleaned_data.get('status')
-        self.product.TYPE_CHOICES = self.cleaned_data.get('TYPE_CHOICES')
+        type_choice = self.cleaned_data.get('TYPE_CHOICES')
+        print('TYPE CHOICESSSSSSSSSSSSS')
+        print(type_choice)
         self.product.description = self.cleaned_data.get('description')
         self.product.category = self.cleaned_data.get('category')
         self.product.price = self.cleaned_data.get('price')
+
+        print('LOOK AT MEEEEEEEEEEEE')
+        print(self.product.TYPE_CHOICES)
+
+        if type_choice == 'BulkProduct':
+            self.product.quantity = self.cleaned_data.get('quantity')
+            self.product.polymorphic_ctype.model = type_choice
+
+        if type_choice == 'RentalProduct':
+            self.product.max_rental_days = self.cleaned_data.get('max_rental_days')
+            self.product.retire_date = self.cleaned_data.get('retire_date')
+            self.product.polymorphic_ctype.model = type_choice
 
         self.product.save()
 
